a1f1e0a13b015183e48f1815986b3670f7b2457f
Andrew Bueide <abueide@protonmail.com>
2016-10-15 00:37:06 -0600

    Deletes downloaded files on close

--- src/main/Main.java
+++ src/main/Main.java
@@ -3,6 +3,11 @@ package main;
 import controller.MainController;
 import javafx.application.Application;
 import javafx.stage.Stage;
+import org.apache.commons.io.FileUtils;
+import util.Util;
+
+import java.io.File;
+import java.io.IOException;
 
 public class Main extends Application {
 
@@ -13,6 +18,15 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception {
         new MainController().launch(primaryStage);
+    }
+
+    @Override
+    public void stop(){
+        try {
+            FileUtils.cleanDirectory(new File(Util.getAppData() + "/csv_files/"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
 
